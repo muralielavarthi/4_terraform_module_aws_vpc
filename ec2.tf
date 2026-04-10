3270420ec6d71f5cda4a4ebee9dac1474f8e1d77
Murali Elavarthi <muralis2p@gmail.com>
2026-04-10 16:05:42 +0530

    c

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "instance1" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-    Name = "{local.project}-{local.environment}-{local.application}-instance-public-subnet"
+    Name = local.instance_public_subnet_name
   }
   subnet_id = aws_subnet.public_subnet.id
 }
@@ -13,7 +13,7 @@ resource "aws_instance" "instance2" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
-    Name = "{local.project}-{local.environment}-{local.application}-instance-private-subnet"
+    Name = local.instanc_private_subnet_name
   }
   subnet_id = aws_subnet.private_subnet.id
 }
--- locals.tf
+++ locals.tf
@@ -8,3 +8,11 @@ locals {
 locals {
   bastion_host_name= "{local.project}-{local.environment}-{local.application}-bastion-host"
 }
+
+locals {
+ instance_public_subnet_name = "{local.project}-{local.environment}-{local.application}-instance-public-subnet"
+}
+
+locals {
+  instanc_private_subnet_name= "{local.project}-{local.environment}-{local.application}-instance-private-subnet"
+}
